1a3134981a8282c8fce881cb94df96a957834e3f
T_Mori <morit0301@gmail.com>
2025-06-01 17:11:25 +0900

    MDX のセットアップと About ページの実装

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["tsx", "ts", "jsx", "js", "mdx", "md"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
